1a6557e8c78889f54671b21d2181b6989fde76af
nashihamm <nashihamm@gmail.com>
2025-04-27 16:52:52 +0700

    Add configuration file with environment variables for secret keys and database URI

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-super-secret'
+
+    
