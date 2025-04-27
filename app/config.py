f0132e8fdae7312ca4811d4e379f7531705bc267
nashihamm <nashihamm@gmail.com>
2025-04-27 18:12:19 +0700

    Add email configuration settings to the application config

--- app/config.py
+++ app/config.py
@@ -6,4 +6,11 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-super-secret'
 
-    
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.example.com'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 587
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
