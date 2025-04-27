0ea0f3dede76cda1668ca96ea0f70256646afe0c
nashihamm <nashihamm@gmail.com>
2025-04-27 18:12:36 +0700

    Fix user routes URL prefix by adding leading slash

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from app.controllers.user_controller import UserController
 
 
-user_bp = Blueprint('user', __name__, url_prefix='api/user')
+user_bp = Blueprint('user', __name__, url_prefix='/api/user')
 
 
 @user_bp.route('/<int:user_id>', methods=['GET'])
